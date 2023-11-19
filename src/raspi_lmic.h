4905de69dd074764b54798f853dbd48f7efa0372
xwytdryms <muftifz16@gmail.com>
2023-11-20 06:07:33 +0700

    Buat repo baru isinya LMiC configuration buat Raspberry pi. Sumbernya dari raspi-lmic kemaren + arduino raspi lmic dr Farhan. Baru buat 2 folder di project satu buat otaa, satunya buat spi scan.

---
+++ src/raspi_lmic.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef _RASPI_LMIC_H
+# define _RASPI_LMIC_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "lmic/lmic.h"
+#include "lmic/lmic_bandplan.h"
+#include "lmic/lmic_util.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !_RASPI_LMIC_H
+
+
